fab9bbb501476a9d829f2e64bd0e7a0e441e9d40
Emilymatoke <emilymomata@gmail.com>
2024-05-20 13:29:48 +0300

    Added components

---
+++ src/components/Error404.js
@@ -0,0 +1,16 @@
+export default function Error404(){
+    return(
+        <div className="error404">
+            <div className="error-container"> 
+                <h1> 404 </h1> 
+                <p> 
+                    Oops! The page you're 
+                    looking for is not here. 
+                </p> 
+                <a href="http://localhost:3000/"> 
+                    Go Back to Home 
+                </a> 
+            </div>
+        </div>
+    )
+}
