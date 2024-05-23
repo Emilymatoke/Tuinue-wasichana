209f7ec0b7ae9d39ac9657445617b82120c86f36
Emilymatoke <emilymomata@gmail.com>
2024-05-23 09:48:29 +0300

    Added components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import NavBar from './components/Home';
 import About from './components/About';
 import Donate from './components/Donate';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Adminlogin from './components/Adminlogin';
 import Userlogin from './components/Userlogin';
-
 import './App.css';
+import Adminpage from './components/Adminpage';
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* <NavBar /> */}
+        
         <Routes>
           <Route path="/" element={<NavBar />} />
           <Route path="/about" element={<About />} />
@@ -24,8 +24,9 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/adminlogin" element={<Adminlogin />} />
           <Route path="/userlogin" element={<Userlogin />} />
+          <Route path="/Adminpage" element={<Adminpage />} />
         </Routes>
-        {/* <Footer /> */}
+        
       </div>
     </Router>
   );
